eb061dcc2527e124ed6c4561853b8f97f0eca792
tqq891226 <895099605@qq.com>
2016-11-22 15:03:25 +0800

    movie1

--- app/src/main/java/com/ma/freetravel/url/Url.java
+++ app/src/main/java/com/ma/freetravel/url/Url.java
@@ -11,19 +11,20 @@ public class Url {
     //动态
     public static  String DynamicPath="http://www.qiugonglue.com/api/v3/trends/new_trends_list?client_name=QGLMain&client_version=6.0.1&limit=20&order_by=desc&os_version=17&p=1&platform=android&screen_size=720x1280&uuid=133524328529283&sign=b7b76d8ff653eaa12aed6a0ad698d0af";   //轻松一刻
 
+
     //边走边看
     //轮播
     public  static String Vp_Path="http://apk.zdomo.com/api/ApiFilmAlbum?pagenum=0&pagesize=4&tmp=0.5617492643658829";
+    public static String Head_VpPath="http://apk.zdomo.com/ueditor/net/";
     //轮播点击跳转
     public static String turnPath="http://apk.zdomo.com/api/ApiFilmAlbum?pagenum=0&pagesize=20&tmp=0.6233542039472624";
     //二次跳转     tmp=0.33767933309475884?
     public static String secondTurnPath="http://apk.zdomo.com/api/ApiFilmAlbum/?pageSize=20&pageNum=0&id=110&tmp=0.33767933309475884" ;
-                                        //http://apk.zdomo.com/api/ApiFilmAlbum/?pageSize=20&pageNum=0&id=114&tmp=0.11219674811926295
+    //http://apk.zdomo.com/api/ApiFilmAlbum/?pageSize=20&pageNum=0&id=114&tmp=0.11219674811926295
 
     //listview的网址
     public static String lvPath="http://apk.zdomo.com/api/ApiBasic?pageSize=20&pageNum=0&tmp=0.06086050612584448";
     //listvie点击跳转 第二页的内容需要xml解析listview的Content属性
-
     //目的地
     //十一必玩
     public static String PlayPath="http://www.qiugonglue.com/api/v3/place/nav_place_list?client_name=QGLMain&client_version=6.0.1&os_version=17&platform=android&screen_size=720x1280&tm=1479727833223&type=hot&type_value=1&uuid=133524328529283&sign=a6fe988a3137dd395d0e16f6d2506df2";
