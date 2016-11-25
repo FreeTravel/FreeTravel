431c4a41f4dd628d4b9746c65c3333750dacfe82
ZERAL <zhaojifengeral@163.com>
2016-11-25 11:30:48 +0800

    一些简单修改

--- app/src/main/java/com/ma/freetravel/fragment/HomeFragment.java
+++ app/src/main/java/com/ma/freetravel/fragment/HomeFragment.java
@@ -36,7 +36,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         mView = (WebView) view.findViewById(R.id.webView);
         WebSettings settings = mView.getSettings();
